da32942ac28a50d92ff53ae9b91bf490631c41dd
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2021-03-24 08:00:44 -0600

    Modify scripts to extract multiple files.

--- get_sarp_chr.sh
+++ get_sarp_chr.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
-chr=$1
-echo "Processing chromosome $chr ..."
-./bcftools view -S SARP.txt "/Volumes/Promise Pegasus/dbgap_downloads/78082/topmed-dcc/exchange/phs001446_TOPMed_WGS_SARP/Combined_Study_Data/Genotypes/freeze.8/phased/freeze.8.chr${chr}.pass_only.phased.bcf" | bgzip -c > "/Volumes/Promise Pegasus/topmed_freeze8_phased_sarp/chr${chr}.vcf.gz"
-tabix "/Volumes/Promise Pegasus/topmed_freeze8_phased_sarp/chr${chr}.vcf.gz"
+data_dir="/Volumes/Promise Pegasus/dbgap_downloads/78082/topmed-dcc/exchange/phs001446_TOPMed_WGS_SARP/Combined_Study_Data/Genotypes/freeze.8/phased/"
+out_dir="/Volumes/Promise Pegasus/topmed_freeze8_phased_sarp/"
+
+minsize=100
+for chr in {1..22}; do
+  echo "Processing chromosome $chr ..."
+  size=$(stat -f%z "${out_dir}/chr${chr}.vcf.gz")
+
+  if (( size > minsize )); then
+    echo "Skipping chr $chr appears to be extracted"
+    continue
+  fi
+
+  # echo ./bcftools view -S SARP.txt "${data_dir}/freeze.8.chr${chr}.pass_only.phased.bcf"
+  # echo bgzip -c "${out_dir}/chr${chr}.vcf.gz"
+
+  ./bcftools view -S SARP.txt "${data_dir}/freeze.8.chr${chr}.pass_only.phased.bcf" |\
+    bgzip -c > "${out_dir}/chr${chr}.vcf.gz"
+
+  # echo tabix "/Volumes/Promise Pegasus/topmed_freeze8_phased_sarp/chr${chr}.vcf.gz"
+
+  tabix "/Volumes/Promise Pegasus/topmed_freeze8_phased_sarp/chr${chr}.vcf.gz"
+done 
--- unzip_results.sh
+++ unzip_results.sh
@@ -3,5 +3,8 @@
 zip_password=W1w6VgdSWsyqCr
 
 for ((chr=1; chr<=22; chr++)); do
-    7z e chr_${chr}.zip -y -p$zip_password
+    if [[ -f chr${chr}.dose.vcf.gz ]]; then
+        continue;
+    fi
+    ./7za e chr_${chr}.zip -y -p$zip_password
 done
