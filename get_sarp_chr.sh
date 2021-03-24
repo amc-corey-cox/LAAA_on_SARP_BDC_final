78c62cb490ce07a299509e3ca9f5cd097cf088a8
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2021-03-24 08:02:15 -0600

    Add improved debugging header to scripts.

--- get_sarp_chr.sh
+++ get_sarp_chr.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+#SBATCH -p c2s4
+#SBATCH --job-name=hpc_tmp
+#SBATCH --out=hpc_tmp.log
+#SBATCH --error=hpc_tmp.err
 
+# These lines generally make bash shell scripts safer
+set -euo pipefail        # -e: exit on any error, -u: treat unset variables as error
+IFS="`printf '\n\t'`"    # split words only on \n and \t, not space (improves loops)
+
+# Uncomment this for better logging
+set -x # Print each command after variable exansion
+
+# Begin main script
 data_dir="/Volumes/Promise Pegasus/dbgap_downloads/78082/topmed-dcc/exchange/phs001446_TOPMed_WGS_SARP/Combined_Study_Data/Genotypes/freeze.8/phased/"
 out_dir="/Volumes/Promise Pegasus/topmed_freeze8_phased_sarp/"
 
--- unzip_results.sh
+++ unzip_results.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+#SBATCH -p c2s4
+#SBATCH --job-name=hpc_tmp
+#SBATCH --out=hpc_tmp.log
+#SBATCH --error=hpc_tmp.err
 
+# These lines generally make bash shell scripts safer
+set -euo pipefail        # -e: exit on any error, -u: treat unset variables as error
+IFS="`printf '\n\t'`"    # split words only on \n and \t, not space (improves loops)
+
+# Uncomment this for better logging
+set -x # Print each command after variable exansion
+
+# Begin main script
 zip_password=W1w6VgdSWsyqCr
 
 for ((chr=1; chr<=22; chr++)); do
