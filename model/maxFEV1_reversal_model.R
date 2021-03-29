d249d60e4e06647e1341d832ffd80f35f8d48b12
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2021-03-29 17:18:07 -0600

    Fix NA/NaN/Inf Error in maxFEV1_reversal model

--- model/maxFEV1_reversal_model.R
+++ model/maxFEV1_reversal_model.R
@@ -1,22 +1,27 @@
 # Model from SARP-reruns ... admixture mapping
 # log(max_Reversibility.beth.) ~ Age_Enroll + sex + center + height_cm + Baseline_preDrug_FEV1..mL. + RFMIX_genome-wide_African_ancestry
 
-runLaaaModelSummary <- function(model.frame) {
-  # return (summary(lm(Y ~ covariates + allele_dose + afr_dose + allele_afr_dose, data=model.frame)))
-  return (summary(lm(log(max_fev1_reversal) ~ age + sex + center + height_cm + base_FEV1 + allele_dose + afr_dose + allele_afr_dose, data=model.frame)))
+runLaaaModelSummary <- function(model.data.frame) {
+  # return (summary(lm(Y ~ covariates + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame)))
+  # tidyr's drop_na is cleaner than this but tidyr is not in the Docker image.
+  model.data.frame <- dplyr::filter(model.data.frame, ! is.na(max_fev1_reversal) & ! is.na(base_FEV1))
+  return (summary(lm(log(max_fev1_reversal + 1) ~ age + sex + center + height_cm + base_FEV1 + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame)))
 }
 
-runNullModel <- function(model.frame) {
-  # return (lm(Y ~ covariates, data=model.frame))
-  return (lm(log(max_fev1_reversal) ~ age + sex + center + height_cm + base_FEV1, data=model.frame))
+runNullModel <- function(model.data.frame) {
+  # return (lm(Y ~ covariates, data=model.data.frame))
+  model.data.frame <- dplyr::filter(model.data.frame, ! is.na(max_fev1_reversal) & ! is.na(base_FEV1))
+  return (lm(log(max_fev1_reversal + 1) ~ age + sex + center + height_cm + base_FEV1, data=model.data.frame))
 }
 
-runLaaaModel <- function(model.frame) {
-  # return (lm(Y ~ covariates + allele_dose + afr_dose + allele_afr_dose, data=model.frame))
-  return (lm(log(max_fev1_reversal) ~ age + sex + center + height_cm + base_FEV1 + allele_dose + afr_dose + allele_afr_dose, data=model.frame))
+runLaaaModel <- function(model.data.frame) {
+  # return (lm(Y ~ covariates + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame))
+  model.data.frame <- dplyr::filter(model.data.frame, ! is.na(max_fev1_reversal) & ! is.na(base_FEV1))
+  return (lm(log(max_fev1_reversal + 1) ~ age + sex + center + height_cm + base_FEV1 + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame))
 }
 
-runAlleleModelSummary <- function(model.frame) {
-  # return (summary(lm(Y ~ covariates + allele_dose, data=model.frame)))
-  return (summary(lm(log(max_fev1_reversal) ~ age + sex + center + height_cm + base_FEV1 + allele_dose, data=model.frame)))
+runAlleleModelSummary <- function(model.data.frame) {
+  # return (summary(lm(Y ~ covariates + allele_dose, data=model.data.frame)))
+  model.data.frame <- dplyr::filter(model.data.frame, ! is.na(max_fev1_reversal) & ! is.na(base_FEV1))
+  return (summary(lm(log(max_fev1_reversal + 1) ~ age + sex + center + height_cm + base_FEV1 + allele_dose, data=model.data.frame)))
 }
