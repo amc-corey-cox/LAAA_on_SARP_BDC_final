5b794e67bb10bb6c061c0e534c487ab838f3bd97
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2021-03-31 15:53:42 -0600

    Removed unnecessary filtering -happens automatically within lm()

--- model/maxFEV1_reversal_model.R
+++ model/maxFEV1_reversal_model.R
@@ -3,25 +3,20 @@
 
 runLaaaModelSummary <- function(model.data.frame) {
   # return (summary(lm(Y ~ covariates + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame)))
-  # tidyr's drop_na is cleaner than this but tidyr is not in the Docker image.
-  model.data.frame <- dplyr::filter(model.data.frame, ! is.na(max_fev1_reversal) & ! is.na(base_FEV1))
   return (summary(lm(log(max_fev1_reversal + 1) ~ age + sex + center + height_cm + base_FEV1 + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame)))
 }
 
 runNullModel <- function(model.data.frame) {
   # return (lm(Y ~ covariates, data=model.data.frame))
-  model.data.frame <- dplyr::filter(model.data.frame, ! is.na(max_fev1_reversal) & ! is.na(base_FEV1))
   return (lm(log(max_fev1_reversal + 1) ~ age + sex + center + height_cm + base_FEV1, data=model.data.frame))
 }
 
 runLaaaModel <- function(model.data.frame) {
   # return (lm(Y ~ covariates + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame))
-  model.data.frame <- dplyr::filter(model.data.frame, ! is.na(max_fev1_reversal) & ! is.na(base_FEV1))
   return (lm(log(max_fev1_reversal + 1) ~ age + sex + center + height_cm + base_FEV1 + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame))
 }
 
 runAlleleModelSummary <- function(model.data.frame) {
   # return (summary(lm(Y ~ covariates + allele_dose, data=model.data.frame)))
-  model.data.frame <- dplyr::filter(model.data.frame, ! is.na(max_fev1_reversal) & ! is.na(base_FEV1))
   return (summary(lm(log(max_fev1_reversal + 1) ~ age + sex + center + height_cm + base_FEV1 + allele_dose, data=model.data.frame)))
 }
