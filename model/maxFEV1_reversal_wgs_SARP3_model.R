dd1e2304313881ae3ac2dfb4994afe51fc3a0eb8
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2021-04-08 09:54:16 -0600

    Create models for SARP3, removes "+ center" (NA)

---
+++ model/maxFEV1_reversal_wgs_SARP3_model.R
@@ -0,0 +1,23 @@
+# Model from SARP-reruns ... admixture mapping
+# log(max_Reversibility.beth.) ~ Age_Enroll + sex + center + height_cm + Baseline_preDrug_FEV1..mL. + RFMIX_genome-wide_African_ancestry
+# center is NA for SARP3
+
+runLaaaModelSummary <- function(model.data.frame) {
+  # return (summary(lm(Y ~ covariates + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame)))
+  return (summary(lm(log(max_fev1_reversal + 1) ~ age + sex + height_cm + base_FEV1 + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame)))
+}
+
+runNullModel <- function(model.data.frame) {
+  # return (lm(Y ~ covariates, data=model.data.frame))
+  return (lm(log(max_fev1_reversal + 1) ~ age + sex + height_cm + base_FEV1, data=model.data.frame))
+}
+
+runLaaaModel <- function(model.data.frame) {
+  # return (lm(Y ~ covariates + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame))
+  return (lm(log(max_fev1_reversal + 1) ~ age + sex + height_cm + base_FEV1 + allele_dose + afr_dose + allele_afr_dose, data=model.data.frame))
+}
+
+runAlleleModelSummary <- function(model.data.frame) {
+  # return (summary(lm(Y ~ covariates + allele_dose, data=model.data.frame)))
+  return (summary(lm(log(max_fev1_reversal + 1) ~ age + sex + height_cm + base_FEV1 + allele_dose, data=model.data.frame)))
+}
